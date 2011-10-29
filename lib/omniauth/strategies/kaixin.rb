844f2046371fec4ebdabeb8314953967dca723e5
Scott Ballantyne <ussballantyne@gmail.com>
2011-10-29 22:09:06 +0800

    adding back in yuanyis code

--- lib/omniauth/strategies/kaixin.rb
+++ lib/omniauth/strategies/kaixin.rb
@@ -13,20 +13,20 @@ module OmniAuth
         super
       end
 
-      uid { raw_info['id'] }
+      uid { raw_info['uid'] }
 
       info do
         {
-          'nickname' => raw_info['login'],
-          'email' => raw_info['email'],
-          'name' => raw_info['name'],
-          'urls' => {
-            'GitHub' => "https://github.com/#{raw_info['login']}",
-            'Blog' => raw_info['blog'],
-          },
+          :uid => raw_info['uid'],
+          :user_info => raw_info['data']['name'],
+          :location => raw_info['data']['location'],
+          :image => raw_info['data']['head'],
+          :description => raw_info['description'],
+          :extra => {
+            'user_hash' => user_data
+          }
         }
       end
-
       def raw_info
         @raw_info ||= MultiJson.decode(access_token.get("/users/me.json?access_token=#{@access_token.token}").body)
       rescue ::Errno::ETIMEDOUT
@@ -36,3 +36,91 @@ module OmniAuth
   end
 end
 
+OmniAuth.config.add_camelization 'github', 'GitHub'
+
+# require 'omniauth/strategies/oauth2'
+# 
+# module OmniAuth
+#   module Strategies
+# 
+#     # Authenticate to Kaixin001 utilizing OAuth 2.0 and retrieve 
+#     # basic user information.
+#     #
+#     # OAuth 2.0 - Kaixin001 Documentation 
+#     # http://wiki.open.kaixin001.com/
+#     # 
+#     # Apply kaixin001 key here:
+#     # http://www.kaixin001.com/platform/rapp/rapp.php
+#     # adapted from https://github.com/yzhang/omniauth/commit/eafc5ff8115bcc7d62c461d4774658979dd0a48e    
+# 
+#     class Kaixin < OmniAuth::Strategies::OAuth2
+#       def initialize(*args)
+#         super
+#         # taken from https://github.com/intridea/omniauth/blob/0-3-stable/oa-oauth/lib/omniauth/strategies/oauth/tqq.rb#L15-24
+#         puts options.inspect 
+#         options.client_options = {
+#           :site          => 'https://api.kaixin001.com/',
+#           :authorize_url => '/oauth2/authorize',
+#           :token_url     => '/oauth2/access_token',
+#           :token_method  => :get
+#         }
+#       end
+# 
+#       info do
+#         {
+#           :uid => raw_info['uid'],
+#           :user_info => raw_info['data']['name'],
+#           :location => raw_info['data']['location'],
+#           :image => raw_info['data']['head'],
+#           :description => raw_info['description'],
+#           :extra => {
+#             'user_hash' => user_data,
+#           }
+#         }
+#       end
+# 
+#       extra do
+#         { :raw_info => raw_info }
+#       end
+#       
+#       def callback_phase
+#         
+#         if request.params['error'] || request.params['error_reason']
+#           raise CallbackError.new(request.params['error'], request.params['error_description'] || request.params['error_reason'], request.params['error_uri'])
+#         end
+# 
+#         self.access_token = build_access_token
+#         self.access_token = client.auth_code.refresh_token(access_token.refresh_token) if access_token.expired?
+# 
+#         super
+#       rescue ::OAuth2::Error, CallbackError => e
+#         fail!(:invalid_credentials, e)
+#       rescue ::MultiJson::DecodeError => e
+#         fail!(:invalid_response, e)
+#       rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+#         fail!(:timeout, e)
+#       end
+#       
+#       credentials do
+#         prune!({
+#           'expires' => access_token.expires?,
+#           'expires_at' => access_token.expires_at
+#         })
+#       end
+#       
+#       def user_info
+#         {
+#           'uid'    => raw_info['uid'],
+#           'name'   => raw_info['name'],
+#           'gender' => raw_info['gender'],
+#         }
+#       end
+# 
+#       def raw_info
+#         @raw_info ||= MultiJson.decode(access_token.get("/users/me.json?access_token=#{@access_token.token}").body)
+#       rescue ::Errno::ETIMEDOUT
+#         raise ::Timeout::Error
+#       end
+#     end
+#   end
+# end
